f4f655e30f05d99cc0bdeaad4aa2d3d11232a855
Muh Farid Wajdi A <muhaidil754@gmail.com>
2022-11-28 08:26:53 -0800

    adding overlay colorbox

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import seedsColors from './seedsColors';
 function App() {
   return (
     <div className="App">
-      <Pallets pallets={seedsColors[4]}/>
+      <Pallets pallets={seedsColors[1]}/>
     </div>
   );
 }
