8c6e093def1c7a7846f6d6266b9d00dcaf86fd46
Muh Farid Wajdi A <muhaidil754@gmail.com>
2022-11-28 06:12:12 -0800

    add pallets

---
+++ src/Pallets.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class Pallets extends Component{
+    render() {
+        return (
+            <div className='Pallets'>
+                {/* Navbar */}
+                <div className='Pallets-Colors'>
+                    {/* color Box */}
+
+                </div>
+                {/* Footer */}
+            </div>
+        );
+    }
+}
+export default Pallets
